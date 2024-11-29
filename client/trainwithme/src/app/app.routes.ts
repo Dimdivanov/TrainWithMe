02ca060c75f6530aad5df6fc56cdd8d2e7f756a0
Dimitar Ivanov <riseabove92@yahoo.com>
2024-11-29 14:16:26 +0200

    path to views installed nav menu working

--- client/trainwithme/src/app/app.routes.ts
+++ client/trainwithme/src/app/app.routes.ts
@@ -2,9 +2,18 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './features/auth/components/login/login.component';
 import { RegisterComponent } from './features/auth/components/register/register.component';
 import { DashboardComponent } from './features/dashboard/dashboard.component';
+import { AboutComponent } from './features/about/about.component';
+import { ContactComponent } from './features/contact/contact.component';
+import { HomeComponent } from './features/home/home.component';
+import { TrainerComponent } from './features/trainer/trainer.component';
 
 export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'trainer', component: TrainerComponent },
+  { path: 'contact', component: ContactComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
 ];
---
+++ client/trainwithme/src/app/features/trainer/trainer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trainer',
+  standalone: true,
+  imports: [],
+  templateUrl: './trainer.component.html',
+  styleUrl: './trainer.component.css',
+})
+export class TrainerComponent {}
