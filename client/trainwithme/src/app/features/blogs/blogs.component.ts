d34292791e5fddda6cf37b346356d834060ae5e0
Dimitar Ivanov <riseabove92@yahoo.com>
2024-12-09 16:38:44 +0200

    added loader on blog page

--- client/trainwithme/src/app/features/blogs/blogs.component.ts
+++ client/trainwithme/src/app/features/blogs/blogs.component.ts
@@ -1,20 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { BlogsService } from './blogs.service';
 import { Post } from '../../types/post';
+import { LoaderComponent } from '../../shared/loader/loader.component';
 
 @Component({
   selector: 'app-blogs',
   standalone: true,
-  imports: [],
+  imports: [LoaderComponent],
   providers: [BlogsService],
   templateUrl: './blogs.component.html',
   styleUrl: './blogs.component.css',
 })
 export class BlogsComponent implements OnInit {
   blogs: Post[] = [];
+
+  //testing loader
+  isLoading = true;
+
   constructor(private blogsService: BlogsService) {}
   ngOnInit(): void {
     this.fetchItems();
+    //testing loader
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 2000);
   }
 
   fetchItems(): void {
