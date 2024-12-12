f6feaa850f6483fb414d7998ddf05b4451010a25
Dimitar Ivanov <riseabove92@yahoo.com>
2024-12-12 13:59:17 +0200

    welcome component added to main page, structure/style ready

--- client/trainwithme/src/app/features/home/home.component.ts
+++ client/trainwithme/src/app/features/home/home.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
-import { HeroSectionComponent } from "./hero-section/hero-section.component";
-import { BlogPostsSectionComponent } from "./blog-posts-section/blog-posts-section.component";
+import { HeroSectionComponent } from './hero-section/hero-section.component';
+import { BlogPostsSectionComponent } from './blog-posts-section/blog-posts-section.component';
+import { WelcomeMsgComponent } from '../../shared/welcome-msg/welcome-msg.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeroSectionComponent, BlogPostsSectionComponent],
+  imports: [
+    HeroSectionComponent,
+    WelcomeMsgComponent,
+    BlogPostsSectionComponent,
+  ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
---
+++ client/trainwithme/src/app/shared/welcome-msg/welcome-msg.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-welcome-msg',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './welcome-msg.component.html',
+  styleUrl: './welcome-msg.component.css'
+})
+export class WelcomeMsgComponent {
+
+}
