7df08bc642e6c070b7ddff87f9a21d17a4b45a74
Dimitar Ivanov <riseabove92@yahoo.com>
2024-12-12 17:14:29 +0200

    about updated, footer updated, start now component created

--- client/trainwithme/src/app/features/about/about.component.ts
+++ client/trainwithme/src/app/features/about/about.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { StartNowComponent } from '../../shared/start-now/start-now.component';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [StartNowComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
---
+++ client/trainwithme/src/app/shared/start-now/start-now.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-start-now',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './start-now.component.html',
+  styleUrl: './start-now.component.css'
+})
+export class StartNowComponent {
+
+}
